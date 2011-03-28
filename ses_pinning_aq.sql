ef1da04646eee520aeecd41127a779ad0abb89b7
Jeremy Baumont <jeremy@baumont.org>
2011-03-28 21:49:37 +0200

    Add 3 scripts over indexes, execution plan and pinning sessions
    Adding 3 scripts:
    - eph.sql: about the execution plan stored in history view
      and display them
    - ind9_struct_stats.sql: display summary of information over the
      structure of an index and the statistics if analyzed
    - ses_pinning_aq.sql: troubleshoot a deadlock situation with owb over
      sessions pinning the advanced queue package

---
+++ ses_pinning_aq.sql
@@ -0,0 +1,61 @@
+--------------------------------------------------------------------------------     
+--    
+-- File name:   ses_pinning_aq.sql
+-- Purpose:     identify user sessions pinning Advanced Queue objects
+--
+--		Used to troubleshoot on OWB the ORA-04020 deadlock
+--		detected while trying to lock object or ORA-04021
+--		timeout occured while waiting to lock object
+--		See Oracle documentation for details:
+--              http://download.oracle.com/docs/cd/B31080_01/doc/install.102/b28224/diagnose_06.htm              
+--
+-- Author:      Jeremy Baumont
+-- Copyright:   (c) http://www.jeremy.baumont.org
+--                                                                                  
+-- Usage:       @ses_pinning_aq.sql>
+--------------------------------------------------------------------------------
+column s.sid format a5
+column s.serial# format a8
+column s.username format a10
+column objectname format a10
+
+
+prompt " User session pinning Advanced Queue objets "
+select distinct (s.sid),
+    s.serial#,
+    s.username,
+    x.kglnaobj as objectname
+from 
+    dba_kgllock l,
+    v$session s,
+    x$kgllk x
+where
+    l.kgllktype = 'Pin' and
+    s.saddr = l.kgllkuse and
+    s.saddr = x.kgllkuse and
+    x.kglnaobj in ('DBMS_AQ', 'DBMS_AQADM');
+
+prompt " Alter kill sqls corresponding to kill previous sessions "
+set heading off
+set feedback off
+prompt " spooling in /var/tmp/kill_ses_pinning_aq.sql "
+spool /var/tmp/kill_ses_pinning_aq.sql
+select 
+	'ALTER SYSTEM KILL SESSION '''
+	|| ssid 
+	||','
+	||sserial# || ''';'
+from(
+select distinct(s.sid) as ssid,
+	s.serial# as sserial#
+from
+    dba_kgllock l,
+    v$session s,
+    x$kgllk x
+where
+    l.kgllktype = 'Pin' and
+    s.saddr = l.kgllkuse and
+    s.saddr = x.kgllkuse and
+    x.kglnaobj in ('DBMS_AQ', 'DBMS_AQADM')
+);
+spool off
